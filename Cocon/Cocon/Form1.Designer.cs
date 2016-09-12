af178026a9f631ba42064543a5a76c189a740ce1
phomm <phomm@mail.ru>
2016-09-12 16:03:54 +0500

    basic

---
+++ Cocon/Cocon/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Cocon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toollabelCount = new System.Windows.Forms.ToolStripLabel();
+            this.toolbtnOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolbtnMatrixDrawingMajority = new System.Windows.Forms.ToolStripButton();
+            this.toollabelRows = new System.Windows.Forms.ToolStripLabel();
+            this.toolcomboRows = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolcomboSize = new System.Windows.Forms.ToolStripComboBox();
+            this.toolbtnGenerate = new System.Windows.Forms.ToolStripButton();
+            this.Zone = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ZoneHolder = new System.Windows.Forms.Panel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Zone)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.ZoneHolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toollabelCount,
+            this.toolbtnOpen,
+            this.toolbtnMatrixDrawingMajority,
+            this.toollabelRows,
+            this.toolcomboRows,
+            this.toolStripLabel1,
+            this.toolcomboSize,
+            this.toolbtnGenerate});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(551, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toollabelCount
+            // 
+            this.toollabelCount.Name = "toollabelCount";
+            this.toollabelCount.Size = new System.Drawing.Size(40, 22);
+            this.toollabelCount.Text = "Count";
+            // 
+            // toolbtnOpen
+            // 
+            this.toolbtnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtnOpen.Name = "toolbtnOpen";
+            this.toolbtnOpen.Size = new System.Drawing.Size(40, 22);
+            this.toolbtnOpen.Text = "Open";
+            this.toolbtnOpen.Click += new System.EventHandler(this.toolbtnOpen_Click);
+            // 
+            // toolbtnMatrixDrawingMajority
+            // 
+            this.toolbtnMatrixDrawingMajority.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtnMatrixDrawingMajority.Name = "toolbtnMatrixDrawingMajority";
+            this.toolbtnMatrixDrawingMajority.Size = new System.Drawing.Size(109, 22);
+            this.toolbtnMatrixDrawingMajority.Text = "RowMajorDrawing";
+            this.toolbtnMatrixDrawingMajority.ToolTipText = "Vertical Fill instead of usual Horizontal";
+            this.toolbtnMatrixDrawingMajority.Click += new System.EventHandler(this.toolbtnMatrixDrawingMajority_Click);
+            // 
+            // toollabelRows
+            // 
+            this.toollabelRows.Name = "toollabelRows";
+            this.toollabelRows.Size = new System.Drawing.Size(38, 22);
+            this.toollabelRows.Text = "Rows:";
+            // 
+            // toolcomboRows
+            // 
+            this.toolcomboRows.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolcomboRows.Name = "toolcomboRows";
+            this.toolcomboRows.Size = new System.Drawing.Size(75, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(30, 22);
+            this.toolStripLabel1.Text = "Size:";
+            // 
+            // toolcomboSize
+            // 
+            this.toolcomboSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolcomboSize.Name = "toolcomboSize";
+            this.toolcomboSize.Size = new System.Drawing.Size(75, 25);
+            // 
+            // toolbtnGenerate
+            // 
+            this.toolbtnGenerate.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolbtnGenerate.Name = "toolbtnGenerate";
+            this.toolbtnGenerate.Size = new System.Drawing.Size(58, 22);
+            this.toolbtnGenerate.Text = "Generate";
+            this.toolbtnGenerate.Click += new System.EventHandler(this.toolbtnGenerate_Click);
+            // 
+            // Zone
+            // 
+            this.Zone.Location = new System.Drawing.Point(0, 25);
+            this.Zone.Name = "Zone";
+            this.Zone.Size = new System.Drawing.Size(551, 237);
+            this.Zone.TabIndex = 2;
+            this.Zone.TabStop = false;
+            this.Zone.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Zone_MouseMove);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xlsx";
+            this.openFileDialog1.Filter = "xlsx files|*.xlsx";
+            this.openFileDialog1.ShowReadOnly = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(551, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // ZoneHolder
+            // 
+            this.ZoneHolder.AutoScroll = true;
+            this.ZoneHolder.Controls.Add(this.Zone);
+            this.ZoneHolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ZoneHolder.Location = new System.Drawing.Point(0, 25);
+            this.ZoneHolder.Name = "ZoneHolder";
+            this.ZoneHolder.Size = new System.Drawing.Size(551, 215);
+            this.ZoneHolder.TabIndex = 4;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 262);
+            this.Controls.Add(this.ZoneHolder);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Zone)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ZoneHolder.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolbtnOpen;
+        private System.Windows.Forms.ToolStripButton toolbtnMatrixDrawingMajority;
+        private System.Windows.Forms.ToolStripComboBox toolcomboRows;
+        private System.Windows.Forms.ToolStripButton toolbtnGenerate;
+        private System.Windows.Forms.PictureBox Zone;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripLabel toollabelCount;
+        private System.Windows.Forms.ToolStripLabel toollabelRows;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox toolcomboSize;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel ZoneHolder;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+    }
+}
+
---
+++ Cocon/Cocon/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Cocon
+{
+    using Microsoft.Office.Interop.Excel;
+    using Excel = Microsoft.Office.Interop.Excel;
+    using ColorMine;
+    using ColorMine.ColorSpaces;
+    using ColorMine.ColorSpaces.Conversions;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            toolcomboRows.Items.AddRange(RowsValues);
+            toolcomboSize.Items.AddRange(SizeValues);
+            OptionSize = (int)SizeValues[0];
+            OptionRows = (int)RowsValues[0];
+            toollabelCount.Text = "Count: 0";
+            openFileDialog1.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            DoubleBuffered = true;
+            ZoneSize = new Size(Zone.Size.Width, Zone.Size.Height);
+
+        }
+        const string RowMajor = "RowMajorDrawing";
+        const string ColMajor = "ColMajorDrawing";
+
+        private List<Tuple<Lab, Rgb>> Labs = new List<Tuple<Lab, Rgb>>();
+
+        Bitmap bmp = null;
+        Graphics g;
+        bool Generated = false;        
+        bool OptionRowMajor = false;        
+        int optionSize;
+        int optionRows;
+        
+        object[] RowsValues = new object[] { 10, 15, 20, 25, 30 };
+        object[] SizeValues = new object[] { 10, 15, 20, 25, 30 };
+
+        int OptionSize { get { return optionSize; } set { optionSize = value; toolcomboSize.Text = value.ToString(); } }
+        int OptionRows { get { return optionRows; } set { optionRows = value; toolcomboRows.Text = value.ToString(); } }
+
+        Size zoneSize;
+        /// <summary>
+        /// регулирование размеров рабочей области
+        /// </summary>
+        Size ZoneSize
+        {
+            get { return zoneSize; }
+            set
+            {
+                zoneSize = value;
+                if (Zone.Image != null)
+                {
+                    bmp = new Bitmap(Zone.Image);
+                    Zone.Image.Dispose();
+                }
+                else
+                    bmp = new Bitmap(ZoneSize.Width, ZoneSize.Height);
+
+                Zone.Size = ZoneSize;
+                Zone.Image = new Bitmap(ZoneSize.Width, ZoneSize.Height);
+                g = Graphics.FromImage(Zone.Image);
+                g.FillRectangle(new SolidBrush(Color.White), 0, 0, ZoneSize.Width, ZoneSize.Height);
+                g.DrawImage(bmp, 0, 0);
+                g.Dispose();
+                
+            }
+        }
+
+
+        private void toolbtnOpen_Click(object sender, EventArgs e)
+        {
+            List<Tuple<Lab, Rgb>> oldlabs = new List<Tuple<Lab, Rgb>>(Labs);
+            string oldfilename = openFileDialog1.FileName;
+            Excel.Application xlApp = null;
+            Excel.Workbook xlWorkbook = null;
+            Excel._Worksheet xlWorksheet = null;
+            Excel.Range xlRange = null;
+            try
+            {
+                
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    xlApp = new Excel.Application();
+                    xlWorkbook = xlApp.Workbooks.Open(openFileDialog1.FileName, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                    xlWorksheet = (Excel._Worksheet)xlWorkbook.Sheets[1];
+                    xlRange = xlWorksheet.UsedRange;
+                    Labs.Clear();
+                    //CultureInfo cult = new CultureInfo("ru-RU");
+                    for (int r = 2; r <= xlRange.Rows.Count; r++)
+                    {
+                        double l = (double)(xlRange.Cells[r, 1] as Excel.Range).Value2;
+                        double a = (double)(xlRange.Cells[r, 2] as Excel.Range).Value2;
+                        double b = (double)(xlRange.Cells[r, 3] as Excel.Range).Value2;
+                        Labs.Add(new Tuple<Lab, Rgb> (new Lab{ L = l, A = a, B = b}, new Rgb{R = 0, G = 0, B = 0})); 
+                    }
+                    Generated = false;                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Labs = oldlabs;
+                openFileDialog1.FileName = oldfilename;
+                MessageBox.Show("Ошибка чтения файла, предыдущие данные не были изменены. Подробности об ошибке\n" + ex.Message);
+            }
+            finally
+            {
+                Marshal.FinalReleaseComObject(xlRange);
+                Marshal.FinalReleaseComObject(xlWorksheet);
+                xlWorkbook.Close(Type.Missing, Type.Missing, Type.Missing);
+                Marshal.FinalReleaseComObject(xlWorkbook);
+                xlApp.Quit();
+                Marshal.FinalReleaseComObject(xlApp);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            toollabelCount.Text = "Count: " + Labs.Count.ToString();
+        }
+
+        private void toolbtnGenerate_Click(object sender, EventArgs e)
+        {
+            if (Labs.Count == 0)
+                return;
+            int pixelsize = int.Parse(toolcomboSize.Text);
+            OptionSize = pixelsize;
+            int rows = int.Parse(toolcomboRows.Text);
+            OptionRows = rows;
+            int cols = (int)Math.Ceiling((double)Labs.Count / rows);
+            //Bitmap bmp = new Bitmap(cols * pixelsize, rows * pixelsize);
+            //Graphics graf = Graphics.FromImage(bmp);
+            ResizeZone(new System.Drawing.Rectangle(0, 0, cols * pixelsize, rows * pixelsize));
+            bmp = new Bitmap(cols * pixelsize, rows * pixelsize);
+            g = Graphics.FromImage(bmp);
+            bool vert = toolbtnMatrixDrawingMajority.Text == RowMajor;
+            OptionRowMajor = vert;
+            //int labidx = 0;
+
+            //for (int i = 0; i <= (vert ? cols : rows); i++)
+            //    for (int j = 0; j <= (vert ? rows : cols); j++)
+            for (int labidx = 0; labidx < Labs.Count && labidx < 100; labidx++)
+                {
+                    //if (labidx >= Labs.Count || labidx > 100)
+                        
+                    IRgb rgb = Labs[labidx].Item1.To<Rgb>();
+                    Labs[labidx].Item2.R = rgb.R;
+                    Labs[labidx].Item2.G = rgb.G;
+                    Labs[labidx].Item2.B = rgb.B;
+                    Brush brush = new SolidBrush(Color.FromArgb((int)Math.Round(rgb.R), (int)Math.Round(rgb.G), (int)Math.Round(rgb.B)));
+                    int x = vert ? labidx / rows : labidx % cols;
+                    int y = vert ? labidx % rows : labidx / cols;
+                    g.FillRectangle(brush, x * pixelsize, y * pixelsize, pixelsize, pixelsize);
+                }
+            Zone.Image = bmp;
+            //new Bitmap(1, 1)
+                bmp.Save(Path.ChangeExtension(openFileDialog1.FileName, ".bmp"), ImageFormat.Bmp);
+            Generated = true;
+        }
+
+        private void toolbtnMatrixDrawingMajority_Click(object sender, EventArgs e)
+        {
+            toolbtnMatrixDrawingMajority.Text = toolbtnMatrixDrawingMajority.Text == RowMajor ? ColMajor : RowMajor;
+        }
+
+        private void ResizeZone(System.Drawing.Rectangle candidate)
+        {
+            if (ZoneSize.Width < candidate.Right || ZoneSize.Height < candidate.Bottom)
+                ZoneSize = new Size(Math.Max(candidate.Right, ZoneSize.Width),
+                    Math.Max(candidate.Bottom, ZoneSize.Height));
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ResizeZone(ZoneHolder.ClientRectangle);
+        }
+
+        private void Zone_MouseMove(object sender, MouseEventArgs e)
+        {
+            statusStrip1.Items[0].Text = "";
+            if (!Generated)
+                return;
+            int labidx = (OptionRowMajor ? e.X : e.Y) / OptionSize * OptionRows + (OptionRowMajor ? e.Y : e.X) / OptionSize;
+            if (labidx >= Labs.Count)
+                return;
+            Rgb rgb = Labs[labidx].Item2;
+            Lab lab = Labs[labidx].Item1;
+            statusStrip1.Items[0].Text = string.Format("L:{0} A:{1} B:{2} R:{3} G:{4} B:{5}", lab.L, lab.A, lab.B, rgb.R, rgb.G, rgb.B);
+        }
+    }
+}
